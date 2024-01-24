bc956195fa3a4083ecbc0e3cd407e62a2144d76d
DimaGrimach <chiki.briki2@gmail.com>
2024-01-24 21:56:34 +0200

    task #2 finished

--- src/index.ts
+++ src/index.ts
@@ -25,22 +25,18 @@ type  TNewRules = {
 }
 
 function applyCondition(data: IUser[], condition: TConditionsAndData['condition']): IUser[] {
-    if (condition.include) {
+    if (condition.include)
         data = data.filter((item: IUser) => condition.include!.some((rule: IUser) =>
             Object.entries(rule).every(([key, value]) => item[key] === value)));
-        console.log('INC applyCondition-', data);
-    }
 
-    if (condition.exclude) {
+    if (condition.exclude)
         data = data.filter(item => !condition.exclude!.some(rule =>
             Object.entries(rule).every(([key, value]) => item[key] === value)));
-        console.log('EXc applyCondition-', data);
-    }
+
     return data;
 }
 
 function sortByKeys(arrUsers: IUser[], sortKeys: string[]): IUser[] {
-    console.log('sortBy-', sortKeys);
     return arrUsers.sort((aUser: IUser, bUser: IUser): number => {
         for (const sorKey of sortKeys) {
             const comparison: number = String(aUser[sorKey]).localeCompare(String(bUser[sorKey]),
@@ -53,15 +49,12 @@ function sortByKeys(arrUsers: IUser[], sortKeys: string[]): IUser[] {
 
 function tookAndSorting(parsedData: TConditionsAndData, changedRules: string | null): IUser[] {
     let users: IUser[] = parsedData.data;
-    console.log('users-', parsedData.data);
 
     if (parsedData.condition) {
         users = applyCondition(users, parsedData.condition);
     }
-    console.log('Before -', users);
     if (parsedData.condition?.sortBy) {
         users = sortByKeys(users, parsedData.condition.sortBy);
-        console.log('SORTED-', users);
     }
     return users;
 }
@@ -72,12 +65,6 @@ async function getFromConsole(): Promise<Answers> {
             type: 'input',
             name: 'inputData',
             message: 'Enter condition and data in JSON :',
-        },
-        {
-            type: 'input',
-            name: 'additionalRule',
-            message: 'Additional Extension: ',
-            when: (answers: any) => answers.name !== '',
         }
     ])
 }
@@ -94,17 +81,12 @@ async function getFromConsole(): Promise<Answers> {
 
     // Parse the user input
     const inputConditions: TConditionsAndData = JSON.parse(fromConsole.inputData);
-    const conversionRulesParsed: TNewRules | null = fromConsole.additionalRule ? JSON.parse(fromConsole.additionalRule) : null;
 
     // Combine user input with conversion rules with additional rules in file
-    let result: IUser[] = [];
-    if (conversionRulesParsed) {
-        /*temporary empty*/
-    } else
-        result = await tookAndSorting(inputConditions, fromConsole.additionalRule);
+    const result: IUser[] = await tookAndSorting(inputConditions, fromConsole.additionalRule);
 
     // Display the result
-    console.log('{result:',result,'}');
+    console.log('{result:', result, '}');
 }()
 
 
